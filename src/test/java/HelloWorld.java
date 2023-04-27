0fb680d27f1a46304b9b417201fa792ebc794e7a
SergeyDorokhov <dorohsad@mail.ru>
2023-04-27 17:36:31 +0300

    jsonPath()

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,13 +1,22 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
     @Test
     public void testHello() {
-        Response response = RestAssured
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
-                .andReturn();
-        response.prettyPrint();
+                .jsonPath();
+        String answer = response.get("answer");
+        System.out.println(answer);
     }
 }
