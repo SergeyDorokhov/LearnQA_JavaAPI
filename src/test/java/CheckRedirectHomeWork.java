d66a2399680818bb87b01b5127f126aa0a287394
SergeyDorokhov <dorohsad@mail.ru>
2023-05-02 21:06:07 +0300

    CheckRedirect

---
+++ src/test/java/CheckRedirectHomeWork.java
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class CheckRedirectHomeWork {
+    @Test
+    public void testRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        System.out.println(response.getHeader("Location"));
+    }
+}
