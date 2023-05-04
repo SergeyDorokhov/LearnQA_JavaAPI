ef29230dfdbd1b173b1e5f75f303de1c186439f2
SergeyDorokhov <dorohsad@mail.ru>
2023-05-04 11:34:32 +0300

    firstassert

---
+++ src/test/java/FirstAssert.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstAssert {
+    @Test
+    public void testHello() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        assertEquals(200, response.statusCode(), "Unexpected");
+    }
+}
